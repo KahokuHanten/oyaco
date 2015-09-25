da3dfc292a8e547322f7ee9b8f1b99d1bd9b867f
okamuratakeshi <t-okamu@eurus.dti.ne.jp>
2015-09-25 08:28:55 +0000

    Add ranking page

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'rails_12factor', group: :production
 ruby '2.2.2'
+gem 'rakuten_web_service'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'ranking_page' => 'ranking_page#index'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
