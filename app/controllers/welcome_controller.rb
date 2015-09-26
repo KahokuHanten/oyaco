f4a3351e5680c6a39e504a04189dee027de5c135
a1510jy <a1510jy@aiit.ac.jp>
2015-09-26 02:10:19 +0000

    up toppage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+  # GET /
+  def top
+
+    RakutenWebService.configuration do |c|
+      c.application_id = ENV["APPID"]
+      c.affiliate_id = ENV["AFID"]
+    end
+
+    @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 50, :sex => 0)
+  end
 end
