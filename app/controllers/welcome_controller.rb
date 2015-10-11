6ab97de22e58df50c0ffe229d9892bd8c7d7dd58
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-10-11 13:02:25 +0000

    Clean up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,12 @@
 class WelcomeController < ApplicationController
   # GET /
   def top
-    cookies.signed[:dad] = params[:dad]
-    cookies.signed[:mom] = params[:mom]
-    cookies.signed[:pref_id] = params[:pref_id]
-    cookies.signed[:tel] = params[:tel]
+    # Set cookies
+    [:dad, :mom, :pref_id, :tel].each do |param|
+      cookies.signed[param] = params[param]
+    end
 
-    #リクエストパラメータから都道府県コードを取得する
+    # リクエストパラメータから都道府県コードを取得する
     @pref_id = params[:pref_id]
 
     # 誕生日
@@ -20,11 +20,9 @@ class WelcomeController < ApplicationController
     next_birthday = get_next_birthday(birthdate)
     if next_birthday - 1.months < Date.today
       @topics_birthday.push(
-        {
-          title: "もうすぐお父さんの" + (age+1).to_s + "歳の誕生日（" + next_birthday.strftime("%-m月%e日") + ") です",
-          comment: "こんなプレゼントはいかがですか？",
-          items: RakutenWebService::Ichiba::Item.ranking(:age => age_r, :sex => 0)
-        })
+        title: 'もうすぐお父さんの' + (age + 1).to_s + '歳の誕生日（' + next_birthday.strftime('%-m月%e日') + ') です',
+        comment: 'こんなプレゼントはいかがですか？',
+        items: RakutenWebService::Ichiba::Item.ranking(age: age_r, sex: 0))
     end
 
     # 母  FIXME: DRY
@@ -34,55 +32,54 @@ class WelcomeController < ApplicationController
     next_birthday = get_next_birthday(birthdate)
     if next_birthday - 1.months < Date.today
       @topics_birthday.push(
-        {
-          title: "もうすぐお母さんの" + (age+1).to_s + "歳の誕生日（" + next_birthday.strftime("%-m月%e日") + ") です",
-          comment: "こんなプレゼントはいかがですか？",
-          items: RakutenWebService::Ichiba::Item.ranking(:age => age_r, :sex => 1)
-        })
+        title: 'もうすぐお母さんの' + (age + 1).to_s + '歳の誕生日（' + next_birthday.strftime('%-m月%e日') + ') です',
+        comment: 'こんなプレゼントはいかがですか？',
+        items: RakutenWebService::Ichiba::Item.ranking(age: age_r, sex: 1))
     end
 
-    #都道府県コードをもとに都道府県名を取得する
+    # 都道府県コードをもとに都道府県名を取得する
     @pref_name = PrefName.get_pref_name(@pref_id)
 
-    #都道府県コードをもとに警報・注意報を取得する
+    # 都道府県コードをもとに警報・注意報を取得する
     @warnings = LocalInfo.get_weather_warnings(@pref_id)
 
-    #コメントを作る
+    # コメントを作る
     @message = MessageGenerator.new(@warnings).generate
 
-    #祝日情報を取得する
-    @holidays = Holiday.where('holiday_date > ?',Date.today).order('holiday_date')
+    # 祝日情報を取得する
+    @holidays = Holiday.where('holiday_date > ?', Date.today).order('holiday_date')
 
-    #Google search
+    # Google search
     @googlenews = LocalInfo.get_google_news(@pref_name)
 
-    #楽天API呼出し用のIDを環境変数から取得する
+    # 楽天API呼出し用のIDを環境変数から取得する
     RakutenWebService.configuration do |c|
-      c.application_id = ENV["APPID"]
-      c.affiliate_id = ENV["AFID"]
+      c.application_id = ENV['APPID']
+      c.affiliate_id = ENV['AFID']
     end
 
     @tel = params[:tel]
   end
 
-private
+  private
+
   def get_age(birthdate)
-    age = ((Date.today.strftime("%Y%m%d").to_i -
-            Date.strptime(birthdate).strftime("%Y%m%d").to_i)/10000)
+    age = ((Date.today.strftime('%Y%m%d').to_i -
+            Date.strptime(birthdate).strftime('%Y%m%d').to_i) / 10_000)
   end
 
   def get_rakuten_age(age)
     age_r = age.round(-1)
-    if age_r > 50 then age_r = 50 end;
-    if age_r == 0 then age_r = 10 end;
+    age_r = 50 if age_r > 50
+    age_r = 10 if age_r == 0
     age_r
   end
 
   def get_next_birthday(birthdate)
     birthday = Date.strptime(birthdate)
     next_birthday = Date.new(Date.today.year, birthday.month, birthday.day)
-    if (next_birthday < Date.today)
-      next_birthday = Date.new(Date.today.year+1, birthday.month, birthday.day)
+    if next_birthday < Date.today
+      next_birthday = Date.new(Date.today.year + 1, birthday.month, birthday.day)
     end
     next_birthday
   end
