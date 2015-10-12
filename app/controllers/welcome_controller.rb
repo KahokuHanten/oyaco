a60d05c6ffc744d4c1b90aaa441170f1db7e4250
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-10-12 10:52:24 +0000

    Add gems for debugging and set timezone

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,16 @@ class WelcomeController < ApplicationController
       cookies.signed[param] = params[param]
     end
 
+    # 楽天API呼出し用のIDを環境変数から取得する
+    RakutenWebService.configuration do |c|
+      c.application_id = ENV['APPID']
+      c.affiliate_id = ENV['AFID']
+    end
+
     # リクエストパラメータから都道府県コードを取得する
     @pref_id = params[:pref_id]
 
-    # 誕生日
+    # 誕生日の話題
     @topics_birthday = []
 
     # FIXME: need to create model
@@ -37,27 +43,18 @@ class WelcomeController < ApplicationController
         items: RakutenWebService::Ichiba::Item.ranking(age: age_r, sex: 1))
     end
 
-    # 都道府県コードをもとに都道府県名を取得する
+    # 警報・注意報の話題
     @pref_name = PrefName.get_pref_name(@pref_id)
-
-    # 都道府県コードをもとに警報・注意報を取得する
     @warnings = LocalInfo.get_weather_warnings(@pref_id)
-
-    # コメントを作る
     @message = MessageGenerator.new(@warnings).generate
 
-    # 祝日情報を取得する
+    # 祝日関連の話題
     @holidays = Holiday.where('holiday_date > ?', Date.today).order('holiday_date')
 
     # Google search
     @googlenews = LocalInfo.get_google_news(@pref_name)
 
-    # 楽天API呼出し用のIDを環境変数から取得する
-    RakutenWebService.configuration do |c|
-      c.application_id = ENV['APPID']
-      c.affiliate_id = ENV['AFID']
-    end
-
+    # 電話番号
     @tel = params[:tel]
   end
 
