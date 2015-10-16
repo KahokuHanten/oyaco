aca223a17e4b0e9116152ef560d4c3d3f066c6f6
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-10-16 16:03:46 +0000

    Fix db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# 2015
+# 感謝しましたか
+Holiday.create(name: '敬老の日', date: '2015-09-21')
+Holiday.create(name: '父の日', date: '2015-06-21')
+Holiday.create(name: '母の日', date: '2015-05-10')
+# 帰省しましたか
+Holiday.create(name: '元日', date: '2015-01-01')
+Holiday.create(name: '春分の日', date: '2015-03-21')
+Holiday.create(name: '秋分の日', date: '2015-09-23')
 
-#2015
-#感謝しましたか
-Holiday.create(name => '敬老の日', date => '2015-09-21')
-Holiday.create(name => '父の日', date => '2015-06-21')
-Holiday.create(name => '母の日', date => '2015-05-10')
-#帰省しましたか
-Holiday.create(name => '元日', date => '2015-01-01')
-Holiday.create(name => '春分の日', date => '2015-03-21')
-Holiday.create(name => '秋分の日', date => '2015-09-23')
-
-#2016
-#感謝しましたか
-Holiday.create(name => '敬老の日', date => '2016-09-19')
-Holiday.create(name => '父の日', date => '2016-06-19')
-Holiday.create(name => '母の日', date => '2016-05-08')
-#帰省しましたか
-Holiday.create(name => '元日', date => '2016-01-01')
-Holiday.create(name => '春分の日', date => '2016-03-20')
-Holiday.create(name => '秋分の日', date => '2016-09-22')
+# 2016
+# 感謝しましたか
+Holiday.create(name: '敬老の日', date: '2016-09-19')
+Holiday.create(name: '父の日', date: '2016-06-19')
+Holiday.create(name: '母の日', date: '2016-05-08')
+# 帰省しましたか
+Holiday.create(name: '元日', date: '2016-01-01')
+Holiday.create(name: '春分の日', date: '2016-03-20')
+Holiday.create(name: '秋分の日', date: '2016-09-22')
