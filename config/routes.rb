228c8cd4697ae89f69a0bb08ae8712e9ee35d78b
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-10-30 08:24:25 +0000

    Update tests for devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :people
   resources :events
-  resources :users
+  scope "/admin" do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
