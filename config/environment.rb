6a86ba608eefb6a117c3025ad4732463d5fb8bd2
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-11-03 12:41:34 +0000

    Setup sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,13 @@ Rails.application.initialize!
 require 'local_info'
 require 'pref_name'
 require 'hobby'
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
