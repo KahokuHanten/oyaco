9ff493d54975cabfa47aa85f35b495a0bd908d9d
s.ishikawa <s.ishikawa.gojo@gmail.com>
2015-11-06 15:26:06 +0000

    add user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should user name valid" do
+    user = User.create(name: 'テスト太郎',email:'oyaco@oyaco.com',password:'testtest')
+    assert user.valid?
+  end
+
+  test "should user name invalid" do
+    invalid_name = 'テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テスト太郎テ'
+    user = User.create(name: invalid_name ,email:'oyaco@oyaco.com',password:'testtest')
+    assert user.invalid?
+  end
+
 end
