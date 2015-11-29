3b2b086949de8d4bf325b01de676ab0e9259f5e6
Takashi Sasai <SASAI.Takashi@gmail.com>
2015-11-29 11:33:23 +0000

    Fix event form
    - Show failure and success
    - Use modal dialog
    - Use simple_form
    - Use bootstrap for confirm

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,9 @@
 //= require_tree .
 //= require bootstrap-sprockets
 //= require moment
+//= require moment/ja
 //= require bootstrap-datetimepicker
+//= require pickers
+//= require twitter/bootstrap/rails/confirm
+
+$.fn.twitter_bootstrap_confirmbox.defaults.title = "記念日を削除";
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,11 @@ class EventsController < ApplicationController
   def create
     @user = current_user
     @event = @user.events.create(event_params)
+    if @event.errors.present?
+      flash[:alert] = "登録できませんでした #{@event.errors.full_messages.join(" ")}"
+    else
+      flash[:notice] = "登録しました"
+    end
     redirect_to home_path
   end
 
